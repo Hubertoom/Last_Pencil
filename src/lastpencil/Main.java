51ca1096a8c541176814ddbc112faac5c4d016ce
Hubertoom <h.michalec@o2.pl>
2023-06-19 23:33:49 +0200

    stage_2 all test passed

--- src/lastpencil/Main.java
+++ src/lastpencil/Main.java
@@ -2,7 +2,6 @@ package lastpencil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("|".repeat(5));
-        System.out.println("Your turn!");
+        new Game().startGame();
     }
 }

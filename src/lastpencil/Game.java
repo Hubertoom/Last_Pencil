5eaadc0bdeb277746bc60f79f2b5b13fe79a3abe
Hubertoom <h.michalec@o2.pl>
2023-06-19 23:35:52 +0200

    stage_3 all test passed

--- src/lastpencil/Game.java
+++ src/lastpencil/Game.java
@@ -8,11 +8,16 @@ public class Game {
     public void startGame() {
         System.out.println("How many pencils would you like to use:");
         int numberOfPencils = Integer.parseInt(scanner.nextLine());
-
         System.out.println("Who will be the first (John, Jack):");
         String firstPlayer = scanner.nextLine();
+        String secondPlayer = firstPlayer.equals("John") ? "Jack" : "John";
+        boolean firstPlayerTurn = true;
 
-        System.out.println("|".repeat(numberOfPencils));
-        System.out.println(firstPlayer + " is going first!");
+        while (numberOfPencils > 0) {
+            System.out.println("|".repeat(numberOfPencils));
+            System.out.printf("%s's turn:\n", firstPlayerTurn ? firstPlayer : secondPlayer);
+            numberOfPencils -= Integer.parseInt(scanner.nextLine());
+            firstPlayerTurn = !firstPlayerTurn;
+        }
     }
 }
